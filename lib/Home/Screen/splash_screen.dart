7804d3bcedb12d0f4d45b6216502c6aafcfb9c14
RohitSth <shrestha.rohit655@gmail.com>
2024-08-05 23:08:49 +0545

    rm :: removed unnecessary codes

--- lib/Home/Screen/splash_screen.dart
+++ lib/Home/Screen/splash_screen.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:flutter_xploverse/Home/Screen/main_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_xploverse/Authentication/Screen/login.dart';
 import 'package:flutter_xploverse/Home/Screen/home_screen.dart';
@@ -53,18 +52,6 @@ class _SplashScreenState extends State<SplashScreen>
               );
             },
           ),
-          // PageRouteBuilder(
-          //   pageBuilder: (context, animation, secondaryAnimation) =>
-          //       MainScreen(),
-          //   transitionDuration: const Duration(milliseconds: 800),
-          //   transitionsBuilder:
-          //       (context, animation, secondaryAnimation, child) {
-          //     return FadeTransition(
-          //       opacity: animation,
-          //       child: child,
-          //     );
-          //   },
-          // ),
         );
       });
     });
