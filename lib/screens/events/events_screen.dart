db335818ea9c96f515be3cb226a1e2c9eb8c1eb7
RohitSth <shrestha.rohit655@gmail.com>
2024-08-06 16:46:25 +0545

    update :: updated the view of events screen and added padding at the bottom

--- lib/screens/events/events_screen.dart
+++ lib/screens/events/events_screen.dart
@@ -17,7 +17,8 @@ class EventsScreen extends ConsumerWidget {
         children: [
           Expanded(
             child: GridView.builder(
-              padding: const EdgeInsets.all(16),
+              padding: const EdgeInsets.only(
+                  left: 16, top: 16, right: 16, bottom: 100),
               itemCount: allEvents.length,
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
